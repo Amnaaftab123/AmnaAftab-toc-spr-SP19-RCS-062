969674de45d01def31e5c939dcee763664537e74
Amnaaftab123 <65035812+Amnaaftab123@users.noreply.github.com>
2020-08-06 23:33:48 +0500

    ....
    Final project

---
+++ AmnaAftab_Assignment5B/Assets/RollAball/Scripts/BoxSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    //All locations in the scene
+    public Transform[] RandomLocations;
+
+    //Prefab of Box
+    public GameObject BoxPrefab;
+
+    //can edit from inspector
+    public int _NoBoxesToSpawn = 20;
+
+    [HideInInspector]
+    public bool[] Usedlocation;
+    public static int S_no =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Usedlocation = new bool[RandomLocations.Length];
+        S_no = 0;
+        //for (int i = 0; i < RandomLocations.Length; i++)
+        //{
+        //    Usedlocation[i] = false;
+        //}
+
+        for (int i = 0; i < _NoBoxesToSpawn; i++)
+        {
+            checkAgain:
+            int randLocation = Random.Range(0, RandomLocations.Length);
+
+            if (!Usedlocation[randLocation])
+            {
+                Usedlocation[randLocation] = true;
+                var box = Instantiate(BoxPrefab, RandomLocations[randLocation].position, Quaternion.identity);
+                //if (i > 12)
+                //{
+                //    box.GetComponent<Rotator>().palindrom += S_no;
+                //}
+
+            }
+            else
+            {
+                goto checkAgain;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AmnaAftab_Assignment5B/Assets/RollAball/Scripts/Rotator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Rotator : MonoBehaviour
+{
+    public string palindrom;
+    //when you see this text me!!
+    private void Start()
+    {
+        BoxSpawner.S_no++;
+
+        float RandNo = Random.Range(0f, 10f);
+        palindrom = (int)RandNo + "a" + 2;
+
+        var inversed = new string(palindrom.ToCharArray().Reverse().ToArray());
+        palindrom = palindrom + inversed;
+        inversed = new string(palindrom.ToCharArray().Reverse().ToArray());
+        if (BoxSpawner.S_no > 12)
+            palindrom = palindrom + inversed + BoxSpawner.S_no;
+        else
+            palindrom = palindrom + inversed;
+
+        transform.GetChild(0).GetComponent<TextMesh>().text = palindrom;
+
+        //print(palindrom);
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,30 * Time.deltaTime, 0) );
+    }
+}
